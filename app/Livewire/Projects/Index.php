a52275264d0c852f4888a3c7c5e7d3b47c51267a
iscmarkoantonio <iscmarko2021@gmail.com>
2025-10-05 15:09:59 -0600

    Diseño de Tabla de Proyectos y listar proyectos con insignia de estado y paginación

--- app/Livewire/Projects/Index.php
+++ app/Livewire/Projects/Index.php
@@ -2,12 +2,24 @@
 
 namespace App\Livewire\Projects;
 
+use App\Services\ProjectService;
 use Livewire\Component;
+use Livewire\WithoutUrlPagination;
+use Livewire\WithPagination;
 
 class Index extends Component
 {
-    public function render()
+
+    use WithPagination, WithoutUrlPagination;
+
+    public function getAllProjects(ProjectService $projectService)
     {
-        return view('livewire.projects.index');
+        return $projectService->getAllProjects()->orderBy('id', 'DESC')->paginate(10);
+    }
+
+    public function render(ProjectService $projectService)
+    {
+        $projects = $this->getAllProjects($projectService);
+        return view('livewire.projects.index', compact('projects'));
     }
 }
--- resources/views/livewire/projects/index.blade.php
+++ resources/views/livewire/projects/index.blade.php
@@ -40,7 +40,55 @@
     </div>
 
     {{-- Table for listing --}}
+    <div class="overflow-x-auto border rounded-xl shadow-md">
+        <table class="min-w-full table-auto text-sm text-left">
+            <thead class="bg-gray-50 text-gray-700 uppercase text-xs font-semibold border-b">
+                <tr>
+                    <th class="py-4">#</th>
+                    <th class="py-4">Name</th>
+                    <th class="py-4">Description</th>
+                    <th class="py-4">Status</th>
+                    <th class="py-4">Deadline</th>
+                    <th class="px-13 py-4">Logo</th>
+                    <th class="py-4 text-center">Actions</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                @forelse ($projects as $project)
+                    <tr class="hover:bg-gray-50 transition">
+                        <td class="py-4">{{ $loop->index + 1 }}</td>
+                        <td class="py-4">{{ $project->name }}</td>
+                        <td class="py-4">{{ $project->description }}</td>
+                        <td class="py-4">{{ $project->status }}</td>
+                        <td class="py-4">{{ $project->deadline }}</td>
+                        <td class="py-4">
+                            @if ($project->project_logo)
+                                <img src="{{ asset('storage/' . $project->project_logo) }}" alt="Project logo"
+                                    class="h-18 w-32 rounded border" />
+                            @endif
+                        </td>
+
+                        {{-- Actions --}}
+
+                        <td class="py-4">
+
+                        </td>
+                    </tr>
+                @empty
+                    <tr>
+                        <td colspan="7" class="p-6 text-center">
+                            <flux:text class="flex items-center justify-center text-red-500">
+                                <flux:icon.exclamation-triangle class="mr-2" /> No projects found!
+                            </flux:text>
+                        </td>
+                    </tr>
+                @endforelse
 
 
+
+            </tbody>
+        </table>
+    </div>
 
 </div>
